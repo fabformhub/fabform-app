07fe69a13cb2bc6a2f662f925a8588718fb9c7be
Geoffrey Callaghan <geoffrey.callaghan@proton.me>
2025-08-11 13:12:07 +0100

    ok

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://lbwtzoxtlssyjeututrs.supabase.co.supabase.co'
+const supabaseUrl = 'https://lbwtzoxtlssyjeututrs.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxid3R6b3h0bHNzeWpldXR1dHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4OTg2OTksImV4cCI6MjA3MDQ3NDY5OX0.GdbcTzNRo5dcuBSCr9_e_HWiXXBJWNm_IYkdwxfbDgI'
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
